a86471385b313e88801e92eb3a99a218fd879c12
Ben <bendelathouwer@users.noreply.github.com>
2020-10-08 21:00:26 +0200

    start

--- src/test1.v
+++ src/test1.v
@@ -1,1 +1,19 @@
-module 
+module test1 (
+    output R0in ,
+    //output B0in ,
+    output R1in ,
+    //output B1in ,
+    //output A0in ,
+    //output A2in ,
+    output SCLKin ,
+    output BLANKin ,
+    //output G0in ,
+    //output G1in ,
+    //output A4in ,
+    //output A1in ,
+    //output A3in,
+    output LATCHin
+);
+
+reg [63:0] data ;
+
