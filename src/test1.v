420aae8633ecc45f6b050197c22294a21f124538
Ben <bendelathouwer@users.noreply.github.com>
2020-10-09 13:18:50 +0200

    code
    verder de code geschreven

--- src/test1.v
+++ src/test1.v
@@ -17,22 +17,34 @@ module test1 (
 );
 
 
-/*integer i;
-initial begin
-  count = 0;
-  for (i=0;i<=1024;i=i+1)
-    Save_state[i] = 0;
-end
-*/
+
 reg [63:0] data ;
-reg [63:0] count1 ;
-reg [63:0] count2 ;
+//reg [63:0] count1 ;
+//reg [63:0] count2 ;
 integer i ; 
+integer j ;
+ //array volstopen met data voor weer te geven op het pannel
 initial begin
-  count = 0 ;
-  for (i=0;i<=63;i=i+1)
-    data[i] = 1;
+  //count = 0 ;
+  for (i = 0 ; i <= 63 ; i = i + 1 )
+  if(i % 2 = 0)//als de counter deelbaar is door 2 is data = 1
+    data[i] = 1 ;
+  else //als counter niet deelbaar is door 2 is data = 0
+   data[i] = 0 ;
   end
 end
+
+always @(posedge clk) begin 
+
+SCLKin <= ~clk;
+	for(j = 0 ; j <= 63 ; j = j + 1 )
+		R0in <= data[j];
+		R1in <= data[j];
+	end
+	LATCHin <= 1 ;
+	
+	
+end
+
 
 endmodule
