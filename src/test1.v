f397c4a41836c317a78214e54391664191f89c7c
Ben <bendelathouwer@users.noreply.github.com>
2020-10-09 12:35:58 +0200

    pcf file aangevuld

--- src/test1.v
+++ src/test1.v
@@ -1,19 +1,38 @@
 module test1 (
+    input clk,
     output R0in ,
-    //output B0in ,
     output R1in ,
-    //output B1in ,
-    //output A0in ,
-    //output A2in ,
-    output SCLKin ,
-    output BLANKin ,
+    //output B0in ,
+   //output B1in ,
     //output G0in ,
     //output G1in ,
-    //output A4in ,
+    //output A0in ,
     //output A1in ,
-    //output A3in,
+     //output A2in ,
+    //output A3in ,
+    //output A4in,
+    output SCLKin ,
+    output BLANKin ,
     output LATCHin
 );
 
-reg [63:0] data ;
 
+/*integer i;
+initial begin
+  count = 0;
+  for (i=0;i<=1024;i=i+1)
+    Save_state[i] = 0;
+end
+*/
+reg [63:0] data ;
+reg [63:0] count1 ;
+reg [63:0] count2 ;
+integer i ; 
+initial begin
+  count = 0 ;
+  for (i=0;i<=63;i=i+1)
+    data[i] = 1;
+  end
+end
+
+endmodule
